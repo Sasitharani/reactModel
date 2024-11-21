6a14869c783bf65dd51af7d9562b43db2564e6be
Sasitharani <50744050+Sasitharani@users.noreply.github.com>
2024-11-21 11:43:07 +0530

    Update server.js one

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
+
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
 dotenv.config(); // Load environment variables from .env file
 
+const app = express(); // Initialize app
+const PORT = process.env.PORT || 3000; // Use the port from environment variables or default to 3000
+
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Middleware to parse JSON bodies
-
-const app = express();
-const PORT = process.env.PORT || 3000; // Use the port from environment variables or default to 3000
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
 app.get('/api/message', (req, res) => {
-  console.log("Api Hit")
+  console.log("API hit successfully");
   res.json({ message: 'Hello from the server!' });
 });
 
